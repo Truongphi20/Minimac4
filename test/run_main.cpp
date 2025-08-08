312eed8118073b87b359f4df1321f4f36f88ae9c
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2025-08-08 16:39:03 +0000

    fix: have to run testcase separately because of writing hijact stdout into files

--- test/run_main.cpp
+++ test/run_main.cpp
@@ -16,32 +16,17 @@ int run_imputation_test(std::vector<std::string> compress_args)
 
     // Run main
     std::time_t start_time = std::time(nullptr);
+
     prog_args args;
     args.parse(static_cast<int>(compress_args.size()), cstrings.data());
-
-    if (args.help_is_set())
-    {
-        args.print_usage(std::cout);
-        return EXIT_SUCCESS;
-    }
-
-    if (args.version_is_set())
-    {
-        std::cout << "minimac v" << VERSION << std::endl;
-        return EXIT_SUCCESS;
-    }
 
     std::cerr << "minimac v" << VERSION << "\n\n";
 
     if (args.update_m3vcf())
-        return convert_old_m3vcf(args.ref_path(), args.out_path(), args.map_path())
-                   ? EXIT_SUCCESS : EXIT_FAILURE;
+        return convert_old_m3vcf(args.ref_path(), args.out_path(), args.map_path()) ? EXIT_SUCCESS : EXIT_FAILURE;
 
     if (args.compress_reference())
-        return compress_reference_panel(args.ref_path(), args.out_path(),
-                                        args.min_block_size(), args.max_block_size(),
-                                        args.slope_unit(), args.map_path())
-                   ? EXIT_SUCCESS : EXIT_FAILURE;
+        return compress_reference_panel(args.ref_path(), args.out_path(), args.min_block_size(), args.max_block_size(), args.slope_unit(), args.map_path()) ? EXIT_SUCCESS : EXIT_FAILURE;
 
     std::uint64_t end_pos = args.region().to();
     std::string chrom = args.region().chromosome();
@@ -53,26 +38,29 @@ int run_imputation_test(std::vector<std::string> compress_args)
         return std::cerr << "Error: could not stat target file\n", EXIT_FAILURE;
 
     dosage_writer output(args.out_path(),
-                         args.emp_out_path(),
-                         args.sites_out_path(),
-                         args.out_format(),
-                         args.out_compression(),
-                         sample_ids,
-                         args.fmt_fields(),
-                         chrom,
-                         args.min_r2(), false);
+        args.emp_out_path(),
+        args.sites_out_path(),
+        args.out_format(),
+        args.out_compression(),
+        sample_ids,
+        args.fmt_fields(),
+        chrom,
+        args.min_r2(), false);
 
     omp::internal::thread_pool2 tpool(args.threads());
     imputation imputer;
-    for (std::uint64_t chunk_start_pos = std::max<std::uint64_t>(1, args.region().from());
-         chunk_start_pos <= end_pos;
-         chunk_start_pos += args.chunk_size())
+    for (std::uint64_t chunk_start_pos = std::max(std::uint64_t(1), args.region().from()); chunk_start_pos <= end_pos; chunk_start_pos += args.chunk_size())
     {
         std::uint64_t chunk_end_pos = std::min(end_pos, chunk_start_pos + args.chunk_size() - 1ul);
-        savvy::region impute_region = {chrom, chunk_start_pos, chunk_end_pos};
+        savvy::region impute_region =
+        {
+            chrom,
+            chunk_start_pos,
+            chunk_end_pos
+        };
 
         if (!imputer.impute_chunk(impute_region, args, tpool, output))
-            return EXIT_FAILURE;
+        return EXIT_FAILURE;
     }
 
     auto total_time = long(std::difftime(std::time(nullptr), start_time));
@@ -82,8 +70,7 @@ int run_imputation_test(std::vector<std::string> compress_args)
     std::fprintf(stderr, "Total time for parsing input: %ld seconds\n", imputer.total_input_time());
     std::fprintf(stderr, "Total time for HMM: %ld seconds\n", imputer.total_impute_time());
     std::fprintf(stderr, "Total time for writing output: %ld seconds\n", imputer.total_output_time());
-    std::fprintf(stderr, "Total wall time (h:mm:ss): %ld:%02ld:%02ld\n",
-                 total_time / 3600, (total_time % 3600) / 60, total_time % 60);
+    std::fprintf(stderr, "Total wall time (h:mm:ss): %ld:%02ld:%02ld\n", total_time / 3600, (total_time % 3600) / 60, total_time % 60);
 
     return EXIT_SUCCESS;
 }
--- test/test_Simple_compress.cpp
+++ test/test_Simple_compress.cpp
@@ -27,30 +27,3 @@ TEST(Simple_run, compress)
     // Restore stdout back to console
     freopen("/dev/tty", "w", stdout);
 }
-
-TEST(Simple_run, impute)
-{
-    // Open file and redirect stdout
-    FILE* fp = freopen("imputed.vcf.gz", "w", stdout);
-
-    // Create args string
-    std::vector<std::string> impute_args{
-        "minimac4",
-        std::string(TEST_DATA) + "/ref_panel.msav",
-        std::string(TEST_DATA) + "/tar_panel.vcf.gz",
-        "-f", "GT",
-        "-O", "vcf.gz",
-        "--temp-buffer", "2"
-    };
-
-    // Run minimac4
-    run_imputation_test(impute_args);
-
-    // Clean up
-    fflush(stdout); // Ensure all output is flushed
-    fclose(fp);     // Close the file
-
-    // Restore stdout back to console
-    freopen("/dev/tty", "w", stdout);
-
-}
